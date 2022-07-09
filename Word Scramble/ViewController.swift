d1d602e1f6a023b3b34cb6ce966571b0db1d3722
koztimesin <a.isumairu01@gmail.com>
2022-07-09 20:48:55 +0300

    added alert controller

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -60,6 +60,9 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -67,9 +70,24 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognized!"
+                    errorMessage = "You can't just make them up."
                 }
+            } else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original!"
             }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title!.lowercased())"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
