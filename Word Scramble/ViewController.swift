0a3e02b97d89ba3289db06015a6c90343c428e47
koztimesin <a.isumairu01@gmail.com>
2022-07-09 13:29:35 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Karthus Saffron on 7/9/22.
+//  Copyright © 2022 koztimesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
